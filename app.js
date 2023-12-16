03a891db10fecd9f55faa105613e586b7c349d36
Azer Heshimov <azrhsimov@gmail.com>
2023-12-16 19:25:15 +0000

    jquery

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 "use strict";
 $(document).ready(function () {
+    $('.btn').click(function (e) {
+        e.preventDefault();
 
+        let inputValue = $('input').val(); 
+
+        let processInput = (inputValue) => {
+            if (isNaN(inputValue)) {
+               
+                $('.col-md-3').append(`<h2>${inputValue}</h2>`);
+            } else {
+                
+                var result = inputValue * inputValue;
+                $('.col-md-3').append(`<h2>${result}</h2>`);
+            }
+        };
+
+        processInput(inputValue);
+    });
 });
