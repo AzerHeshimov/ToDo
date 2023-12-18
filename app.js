295c00ff6ba3c7b28dfcc4771b4470cd27946cda
Azer Heshimov <azrhsimov@gmail.com>
2023-12-18 12:06:10 +0000

    mxwidth

--- app.js
+++ app.js
@@ -5,20 +5,22 @@ $(document).ready(function () {
     $('.btn').click(function (e) {
         e.preventDefault();
 
-        let inputValue = $('input').val();
-
+        let inputValue =$('input').val();
+        $('#myInput').val('');
         if (!isNaN(inputValue)) {
-            // If the input is a number
-            let repeatedValue = '';
+           
+            
             for (let i = 0; i < parseInt(inputValue); i++) {
-                repeatedValue += `<h2>${inputValue}</h2>`;
+                
+                $(".col-md-3").append(`<h2>${inputValue}</h2>`);
             }
             $(".col-md-3").append(repeatedValue);
         } else {
-            // If the input is a string or a combination of letters and numbers
+            
             $(".col-md-3").append(`<h2>${inputValue}</h2>`);
         }
-
-        $('input').val(''); // Clear the input value
+        
+        
     });
 });
+
